bd969aeae6f1adaf78c4085fdb427fdc78f466f8
Forenche <prahul2003@gmail.com>
2021-04-13 11:23:42 +0530

    drone.yml: Update for oos-test branch
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- .circleci/bleed.sh
+++ .circleci/bleed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_surya -b  alpha  kernel
+git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_surya -b  oos-test kernel
 cd kernel
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master gcc64
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master gcc32
